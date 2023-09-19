26f67d29b7fb5e63b1beb7648fc4100706b25ad9
ammann64 <austinmmann64@gmail.com>
2023-09-18 21:09:25 -0500

    add most of the spotify functionality

--- assets/js/spotify.js
+++ assets/js/spotify.js
@@ -1,4 +1,17 @@
-const url = 'https://spotify23.p.rapidapi.com/search/?q=%3CREQUIRED%3E&type=multi&offset=0&limit=10&numberOfTopResults=5';
+let travelTime = 30;
+let genreEl = document.getElementById('genre'); //The genre select element
+let connectSpotifyEl = document.getElementById('connect-spotify'); //The button to search for tracks
+let tracks = [];
+var genreSelected = 'pop';
+let url;
+
+genreEl.addEventListener('change', function(event) {
+	genreSelected = event.target.value;
+	console.log(genreSelected);
+	url = `https://spotify23.p.rapidapi.com/recommendations/?limit=50&seed_genres=${genreSelected}`;
+})
+
+console.log(url);
 const options = {
 	method: 'GET',
 	headers: {
@@ -7,13 +20,38 @@ const options = {
 	}
 };
 
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
+async function searchTracks(thisUrl) {
+	try {
+		const response = await fetch(thisUrl, options);
+		const result = await response.text();
+		console.log(result);
+	} catch (error) {
+		console.error(error);
+	}
 }
+searchTracks(url);
+connectSpotifyEl.addEventListener('click', function(event) {
+	console.log(genreSelected);
+	let resultTracks = [];
+	let tracksDuration = 0;
+	let currentTrackIndex = 0;
+	while (tracksDuration <= travelTime) {
+		let currentTrack = thisResult.tracks[currentTrackIndex];
+		let thisTrack = {
+			name: currentTrack.name,
+			artist: currentTrack.artists[0],
+			duration: DurationInMinutes(currentTrack.duration_ms),
+			url: currentTrack.external_urls[0]
+		};
+		resultTracks.push(thisTrack);
+		currentTrackIndex++;
+		tracksDuration += thisTrack.duration;
+	}
+	console.log(resultTracks);
+})
 
-
-
+function DurationInMinutes(duration) {
+	let durationSeconds = duration / 1000;
+	let durationMinutes = durationSeconds / 60;
+	return durationMinutes;
+}
